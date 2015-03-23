83dd593594e37972c7170d8c26500b0d60e0164e
Gideon Rosales <gideonrosales@gmail.com>
2015-03-23 14:53:05 +0800

    feature: version4

--- index.js
+++ index.js
@@ -14,6 +14,27 @@ server.route( [
 		'handler' : function ( request, reply ) {
 			return reply( 'Hello World' );
 		}
+	},
+	{
+		'method' : 'GET',
+		'path' : '/v2/hello',
+		'handler' : function ( request, reply ) {
+			return reply( 'Hello World version 2' );
+		}
+	},
+	{
+		'method' : 'GET',
+		'path' : '/v3/hello',
+		'handler' : function ( request, reply ) {
+			return reply( 'Hello World version 3' );
+		}
+	},
+	{
+		'method' : 'GET',
+		'path' : '/v4/hello',
+		'handler' : function ( request, reply ) {
+			return reply( 'Hello World version 4' );
+		}
 	}
 ] );
 
