0e12751d8cb73b25b4033fa6c6922200b26a272f
Gideon Rosales <gideonrosales@gmail.com>
2015-03-23 13:36:07 +0800

    feature: web server

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var Hapi = require( 'hapi' );
+var server = new Hapi.Server();
+
+server.connection( {
+	'port' : 9898
+} );
+
+server.route( [
+	{
+		'method' : 'GET',
+		'path' : '/v1/hello',
+		'handler' : function ( request, reply ) {
+			return reply( 'Hello World' );
+		}
+	}
+] );
+
+server.start( function ( error ) {
+
+	if ( error ) {
+		throw error;
+	}
+
+	console.log( 'Server started' );
+} );
