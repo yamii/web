5aedb146be11c413077bb5db8071f6ddd002e169
Miriam Mae Iglesia <miriamiglesia@gmail.com>
2015-04-02 13:24:03 +0800

    test

--- test/indextest.js
+++ test/indextest.js
@@ -9,7 +9,7 @@ require( 'should' );
 		x.should.equal( 0 );
 	} );
 
-	it ( 'should be editing test', function () {
+	it ( 'should be running test', function () {
 		var x = 0;
 		x.should.equal( 0 );
 	} );
